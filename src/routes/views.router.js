c662e6742fc94a4903c4841d5f0809bd8d0ddf0b
wiperalta <wiperalta@gmail.com>
2024-02-22 10:03:51 -0300

    implementacion de login con errores

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -3,18 +3,6 @@ const router = express.Router();
 const ProductManager = require("../dao/db/product-manager-db");
 const productManager = new ProductManager();
 
-// router.get("/", async (req, res) => {
-//     try {
-//         const products = await productManager.getProducts();
-//         const title = "Listado de Productos"
-//         res.render("index", { products, title });
-//     } catch (error) {
-//         res.send({ status: "error", error: error.message });
-//         console.log(error);
-//         return;
-//     }
-// })
-
 router.get("/realtimeproducts", async (req, res) => {
     try {
         res.render("realTimeProducts");
@@ -27,6 +15,37 @@ router.get("/realtimeproducts", async (req, res) => {
 
 router.get("/chat", async (req, res) => {
     res.render("chat");
- })
+})
+
+// Ruta para el formulario de login
+router.get("/login", (req, res) => {
+    // Verifica si el usuario ya está logueado y redirige a la página de perfil si es así
+    if (req.session.login) {
+        return res.redirect("/profile");
+    }
+
+    res.render("login");
+});
+
+// Ruta para el formulario de registro
+router.get("/register", (req, res) => {
+    // Verifica si el usuario ya está logueado y redirige a la página de perfil si es así
+    if (req.session.login) {
+        return res.redirect("/profile");
+    }
+    res.render("register");
+});
+
+// Ruta para la vista de perfil
+router.get("/profile", (req, res) => {
+    // Verifica si el usuario está logueado
+    if (!req.session.login) {
+        // Redirige al formulario de login si no está logueado
+        return res.redirect("/login");
+    }
+
+    // Renderiza la vista de perfil con los datos del usuario
+    res.render("profile", { user: req.session.user });
+});
 
 module.exports = router; 
