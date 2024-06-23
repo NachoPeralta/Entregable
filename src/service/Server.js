ff50b00e202fce6fcfbd15fe3170e9d373779b2d
wiperalta <wiperalta@gmail.com>
2024-06-23 14:53:12 -0300

    correcciones y agregado de logs

--- src/service/Server.js
+++ src/service/Server.js
@@ -67,7 +67,13 @@ class Server {
         // Configuracion de motor de plantilla y handlebars
         this.app.engine("handlebars", hbs.engine);
         this.app.set("view engine", "handlebars");
-        this.app.set("views", path.join(__dirname, "../views"));
+        //this.app.set("views", path.join(__dirname, "../views"));
+
+
+        const viewsPath = path.join(__dirname, "../views");
+        console.log('Current directory:', __dirname);
+        console.log('Views directory:', viewsPath);
+        this.app.set("views", viewsPath);
 
         // Passport
         initializePassport();
